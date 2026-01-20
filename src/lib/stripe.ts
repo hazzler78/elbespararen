4f2315972d9d62dd2f0e26ec38718dd3724a1323
Mikael <hazzler@gmail.com>
2026-01-20 16:51:38 +0100

    Update Stripe API version to 2025-12-15.clover to match TypeScript types

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -49,7 +49,7 @@ export function getStripe(): Stripe | null {
   }
 
   return new Stripe(stripeSecretKey, {
-    apiVersion: '2024-12-18.acacia',
+    apiVersion: '2025-12-15.clover',
     typescript: true,
   });
 }
